4cea427610f44a208a86f94849c48f1d1527b482
Mao Li <63109264+maoli67660@users.noreply.github.com>
2021-01-26 17:46:33 +0000

    Create 4-Merge_Two_Sorted_Lists.py

---
+++ top-interview-questions/3-Linked_List/4-Merge_Two_Sorted_Lists.py
@@ -0,0 +1,27 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
+        head = ListNode(-1)
+        p = head
+        p1 = l1
+        p2 = l2
+        
+        while p1 and p2:
+            if p1.val < p2.val:
+                p.next = p1
+                p1 = p1.next
+            else:
+                p.next = p2
+                p2 = p2.next
+            p = p.next
+            
+        if p1 != None:
+            p.next = p1
+        else:
+            p.next = p2
+        return head.next
+        
