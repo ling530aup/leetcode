6409d61fd5bb35c280983de7e4eb572bd1043552
Mao Li <63109264+maoli67660@users.noreply.github.com>
2021-01-26 22:44:22 +0000

    Update 1-Climbing_Stairs.py

--- top-interview-questions/7-Dynamic_Programming/1-Climbing_Stairs.py
+++ top-interview-questions/7-Dynamic_Programming/1-Climbing_Stairs.py
@@ -1,1 +1,9 @@
-#
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        ls = [1,2]
+        if n > 2:
+            for i in range(n-2):
+                ls.append(ls[-1] + ls[-2])
+            return ls[-1]
+        return ls[n-1]
+      
