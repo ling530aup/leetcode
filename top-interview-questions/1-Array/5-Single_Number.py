21340a1d7b2059875bf86603ebaa02ebe8c38168
Mao Li <63109264+maoli67660@users.noreply.github.com>
2021-02-02 20:44:56 +0000

    Update 5-Single_Number.py

--- top-interview-questions/1-Array/5-Single_Number.py
+++ top-interview-questions/1-Array/5-Single_Number.py
@@ -1,6 +1,6 @@
 from collections import Counter
 
-
+#方法1 懒人做法，调用Counter
 def singleNumber(nums):
     count = Counter(nums)
     for key in count:
@@ -8,7 +8,17 @@ def singleNumber(nums):
             return key
     return -1
 
-
+#方法2
+# 一个集合记录是否访问过元素
+def singleNumber2(nums):
+        _set = set()
+        for item in nums:
+            if item in _set:
+                _set.remove(item)
+            else:
+                _set.add(item)
+        return list(_set)[0]
+    
 if __name__ == '__main__':
     assert singleNumber([4, 1, 2, 1, 2]) == 4
     print('----------------')
