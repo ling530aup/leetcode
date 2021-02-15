5940290835acd22899f6021b59a2f759226006b6
Mao Li <63109264+maoli67660@users.noreply.github.com>
2021-02-15 14:03:32 +0000

    Update 7-Plus_One.py

--- top-interview-questions/Easy/1-Array/7-Plus_One.py
+++ top-interview-questions/Easy/1-Array/7-Plus_One.py
@@ -1,3 +1,17 @@
+'''
+给定一个由 整数 组成的 非空 数组所表示的非负整数，在该数的基础上加一。
+
+最高位数字存放在数组的首位， 数组中每个元素只存储单个数字。
+
+你可以假设除了整数 0 之外，这个整数不会以零开头。
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/plus-one
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+
+
+
+'''
 def plusOne(digits):
     '''
     思路：  类似 数字电路的串行加法器。  用一个flag记录是否有进位，从各位往前面加
@@ -20,4 +34,7 @@ def plusOne(digits):
 
 if __name__ == '__main__':
     assert plusOne([0, 1]) == [0, 2]
-    print('-----plusOne-----------')
+    assert plusOne([4,3,2,1]) == [4,3,2,2]
+    assert plusOne([0]) == [1]
+    assert plusOne([9,9]) == [1,0,0]
+    
