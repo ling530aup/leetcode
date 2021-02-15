e309de3fbf6d86e317d75f3eab9c0f232531d034
Mao Li <63109264+maoli67660@users.noreply.github.com>
2021-02-15 09:52:06 +0000

    Update 2-Best_Time_to_Buy_and_Sell_Stock_II.py

--- top-interview-questions/Easy/1-Array/2-Best_Time_to_Buy_and_Sell_Stock_II.py
+++ top-interview-questions/Easy/1-Array/2-Best_Time_to_Buy_and_Sell_Stock_II.py
@@ -1,3 +1,15 @@
+'''
+Leetcode 122 股票买卖的最佳时间
+给定一个数组，它的第 i 个元素是一支给定股票第 i 天的价格。
+
+设计一个算法来计算你所能获取的最大利润。你可以尽可能地完成更多的交易（多次买卖一支股票）。
+
+注意：你不能同时参与多笔交易（你必须在再次购买前出售掉之前的股票）。
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/best-time-to-buy-and-sell-stock-ii
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+'''
 
 def maxProfit(prices):
     '''
@@ -11,5 +23,7 @@ def maxProfit(prices):
 
 
 if __name__ == '__main__':
-    assert maxProfit([7, 1, 5, 3, 6, 4])
-    print('------')
+    assert maxProfit([7, 1, 5, 3, 6, 4]) == 7  # 1-5   3-6    利润为7
+    assert maxProfit([1,2,3,4,5]) == 7
+    assert maxProfit([7,6,4,3,1])==0   # 没有交易， 零润为 0
+    
