3d26acb2d4579b4d1dee26614950f40ee74f192c
Mao Li <63109264+maoli67660@users.noreply.github.com>
2021-02-15 13:12:41 +0000

    Update 5-Single_Number.py

--- top-interview-questions/Easy/1-Array/5-Single_Number.py
+++ top-interview-questions/Easy/1-Array/5-Single_Number.py
@@ -1,13 +1,21 @@
+'''
+136. 只出现一次的数字 (简单)
+给定一个非空整数数组，除了某个元素只出现一次以外，其余每个元素均出现两次。找出那个只出现了一次的元素。
+
+说明：
+
+你的算法应该具有线性时间复杂度。 你可以不使用额外空间来实现吗？
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/single-number
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+'''
 from collections import Counter
 
 
 # 方法1 懒人做法，调用Counter
 def singleNumber(nums):
-    count = Counter(nums)
-    for key in count:
-        if count[key] == 1:
-            return key
-    return -1
+    return min(Counter(nums).items(), key=lambda x:x[1])
 
 
 # 方法2
@@ -24,4 +32,5 @@ def singleNumber2(nums):
 
 if __name__ == '__main__':
     assert singleNumber([4, 1, 2, 1, 2]) == 4
-    print('----------------')
+    assert singleNumber([2,2,1]） == 1
+                        
