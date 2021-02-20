0f937f4597428419a84da465fafcd745560f3720
Mao Li <63109264+maoli67660@users.noreply.github.com>
2021-02-20 23:14:10 +0000

    Update 5-Kth_Smallest_Element_in_a_BST.py

--- top-interview-questions/Medium/3-Trees and Graphs/5-Kth_Smallest_Element_in_a_BST.py
+++ top-interview-questions/Medium/3-Trees and Graphs/5-Kth_Smallest_Element_in_a_BST.py
@@ -1,1 +1,13 @@
-#
+def inorder(root, ls):
+    if root:
+        inorder(root.left, ls)
+        ls.append(root.val)
+        inorder(root.right, ls)
+
+class Solution:
+    def kthSmallest(self, root: TreeNode, k: int) -> int:
+        ls = []
+        inorder(root, ls)
+        return ls[k-1]
+  
+  
