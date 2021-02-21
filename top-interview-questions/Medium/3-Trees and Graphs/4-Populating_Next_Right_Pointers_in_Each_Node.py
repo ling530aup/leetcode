1ac50c416ba7d50a47a9e2bb4c9cc2092dc75c81
Mao Li <63109264+maoli67660@users.noreply.github.com>
2021-02-21 18:50:28 +0000

    Update 4-Populating_Next_Right_Pointers_in_Each_Node.py

--- top-interview-questions/Medium/3-Trees and Graphs/4-Populating_Next_Right_Pointers_in_Each_Node.py
+++ top-interview-questions/Medium/3-Trees and Graphs/4-Populating_Next_Right_Pointers_in_Each_Node.py
@@ -1,1 +1,29 @@
-#
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val: int = 0, left: 'Node' = None, right: 'Node' = None, next: 'Node' = None):
+        self.val = val
+        self.left = left
+        self.right = right
+        self.next = next
+"""
+from collections import deque
+class Solution:
+    def connect(self, root: 'Node') -> 'Node':
+        if not root:
+            return None
+        
+        queue = deque([root])
+        #visited = set()
+        while queue:
+            current_level_n = len(queue)
+            for i in range(current_level_n):
+                top = queue.popleft()
+                if top.left: queue.append(top.left)
+                if top.right: queue.append(top.right)
+                if i != current_level_n-1:
+                    top.next = queue[0]
+        return root
+                
+        
+        
