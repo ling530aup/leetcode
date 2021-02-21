2cefe14aeb7d6dac1497370afb94967ecf9e5087
Mao li <maoli67660@gmail.com>
2021-02-21 23:52:05 +0000

    add 5-Longest-Palindromic-Substring.py

--- top-interview-questions/Medium/1-Array and Strings/5-Longest-Palindromic-Substring.py
+++ top-interview-questions/Medium/1-Array and Strings/5-Longest-Palindromic-Substring.py
@@ -1,18 +1,39 @@
 def longestPalindrome(s) -> int:
-    longest_p = 1
-    for index, c in enumerate(s):
-        length = min(index, len(s) - index - 1)
-        flag = 1
-        for gap in range(length + 1):
-            if s[index - gap] != s[index + gap]:
+    if len(s) == 1:
+        return s
+
+    longest_p = 0
+    res = ''
+    for center in range(len(s) - 1):
+        length = 0
+
+        while center - length >= 0 and center + length < len(s):
+            if s[center - length] != s[center + length]:
                 break
-            if gap < length and s[index - gap] != s[index + gap + 1]:
-                flag = 1
+            else:
+                if longest_p < length * 2 + 1:
+                    res = s[center - length: center + length + 1]
+                    longest_p = length * 2 + 1
+            length += 1
+
+        # there center is left center
+        length = 0
+        while center - length >= 0 and center + 1 + length < len(s):
+            if s[center - length] != s[center + 1 + length]:
                 break
-        longest_p = max(longest_p, gap * 2 + flag)
-    return longest_p
+            else:
+                if longest_p < length * 2 + 2:
+                    res = s[center - length: center + length + 2]
+                    longest_p = length * 2 + 2
+            length += 1
+    return res
 
 
 if __name__ == '__main__':
-    print(longestPalindrome('aaaa'))
-    assert True
+
+    assert longestPalindrome('babad') == 'bab'
+    assert longestPalindrome('a') == 'a'
+    assert longestPalindrome('aa') == 'aa'
+    assert longestPalindrome('aaa') == 'aaa'
+    assert longestPalindrome('aaaa') == 'aaaa'
+    assert longestPalindrome('caba') == 'aba'
