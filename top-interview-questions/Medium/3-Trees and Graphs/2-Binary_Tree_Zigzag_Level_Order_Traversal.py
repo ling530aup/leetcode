2e5422e9785ea53c6af483dc219f79dbb97ed312
Mao Li <63109264+maoli67660@users.noreply.github.com>
2021-02-21 18:21:51 +0000

    Update 2-Binary_Tree_Zigzag_Level_Order_Traversal.py

--- top-interview-questions/Medium/3-Trees and Graphs/2-Binary_Tree_Zigzag_Level_Order_Traversal.py
+++ top-interview-questions/Medium/3-Trees and Graphs/2-Binary_Tree_Zigzag_Level_Order_Traversal.py
@@ -1,1 +1,35 @@
-#
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+from collections import deque
+
+class Solution:
+    def zigzagLevelOrder(self, root: TreeNode) -> List[List[int]]:
+        if not root:
+            return []
+        res = []
+        queue = deque([root])
+        # visited = set()
+        
+        index = 1
+        while queue:
+            level_node_n = len(queue)
+            current_level = []
+            
+            for _ in range(level_node_n):
+                top = queue.popleft()
+                if top.left: queue.append(top.left)
+                if top.right: queue.append(top.right)
+                current_level.append(top.val)
+                
+            if index % 2:
+                res.append(current_level)
+            else:
+                res.append(current_level[::-1])
+            index +=1 
+        return res
+    
