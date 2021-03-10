cdfd6a9cc86a436c415500ad709f48d8095bd469
Mao Li <63109264+maoli67660@users.noreply.github.com>
2021-03-10 17:24:21 +0000

    Update 1-Remove_Duplicates_from_Sorted_Array.py

--- top-interview-questions/Easy/1-Array/1-Remove_Duplicates_from_Sorted_Array.py
+++ top-interview-questions/Easy/1-Array/1-Remove_Duplicates_from_Sorted_Array.py
@@ -14,15 +14,11 @@
 '''
 
 
-def removeDuplicates(nums):
-    i = 0
-    while i < len(nums) - 1:
-        if nums[i] == nums[i + 1]:
-            nums.remove(nums[i])
-        else:
-            i += 1
-    return len(nums)
-
+def removeDuplicates(self, nums: List[int]) -> int:
+        for i in range(len(nums)-1, 0, -1):
+            if nums[i-1] == nums[i]:
+                del nums[i]
+        return len(nums)
 
 if __name__ == '__main__':
     assert removeDuplicates(nums=[0, 0, 1, 1, 1, 2, 2, 3, 3, 4]) == 5
