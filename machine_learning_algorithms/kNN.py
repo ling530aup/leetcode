c29d8076640af431b5237f8369d457378c456ac9
maoli67660 <maoli67660@gmail.com>
2021-03-10 00:09:24 +0000

    add ML fold.   add first ML algorithm knn.py

---
+++ machine_learning_algorithms/kNN.py
@@ -0,0 +1,18 @@
+from collections import Counter
+import numpy as np
+
+
+class KNearestNeighbor:
+
+    def __init__(self, k=5):
+        self.k = k
+
+    def train(self, X, y):
+        self._X_train = X
+        self._y_train = y
+
+    def predict(self, X_predict):
+        distances = [np.sum((x_train - X_predict) ** 2) ** 0.5 for x_train in self._X_train]
+        topK_index = np.argsort(distances)[:self.k]
+        topK = self._y_train[topK_index]
+        return Counter(topK).most_common(1)[0][0]
