baf209df5a1adb890bc5919e846a3ea6cbf36119
Mao Li <63109264+maoli67660@users.noreply.github.com>
2021-03-29 21:56:00 +0100

    Update 7-Number_of_Islands.py
    Re-write number of island via DFS

--- top-interview-questions/Medium/3-Trees and Graphs/7-Number_of_Islands.py
+++ top-interview-questions/Medium/3-Trees and Graphs/7-Number_of_Islands.py
@@ -1,28 +1,26 @@
-class Solution:
-    def numIslands(self, grid: List[List[str]]) -> int:
-        if not grid or not grid[0]: return 0
-        
-        self.max_x = len(grid)
-        self.max_y = len(grid[0])
-        self.visited = set()
-        self.grid = grid
-        return sum([self.dfs(i,j) for i in range(self.max_x) for j in range(self.max_y)])
-    
-    def dfs(self,i,j):
-        if not self.check(i,j):
-            return 0
-        else:
-            self.visited.add((i,j))
-            self.dfs(i+1,j)
-            self.dfs(i-1,j)
-            self.dfs(i,j+1)
-            self.dfs(i,j-1)
-            return 1
-    
-    
-    def check(self, x,y):
-        if x <0 or x>=self.max_x or y<0 or y>=self.max_y:
-            return False
-        if self.grid[x][y] == '0' or ((x,y) in self.visited):
-            return False
-        return True
+from typing import List
+
+def dfs(grid, x, y):
+    grid[x][y] = 0   # directly use grid to record visited points.
+    if x > 0 and grid[x - 1][y] == "1":
+        dfs(grid, x - 1, y)
+
+    if x < len(grid) - 1 and grid[x + 1][y] == "1":
+        dfs(grid, x + 1, y)
+
+    if y > 0 and grid[x][y - 1] == "1":
+        dfs(grid, x, y - 1)
+
+    if y < len(grid[0]) - 1 and grid[x][y + 1] == "1":
+        dfs(grid, x, y + 1)
+
+
+def numIslands(grid: List[List[str]]) -> int:
+    count = 0
+    for i in range(len(grid)):
+        for j in range(len(grid[0])):
+            if grid[i][j] == "1":
+                count += 1
+                dfs(grid, i, j)
+    return count
+
