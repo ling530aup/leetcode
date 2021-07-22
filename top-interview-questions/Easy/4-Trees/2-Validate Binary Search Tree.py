faa9ab386dcfe3dde2877ab8d64476a23e815646
Mao <li.mao@thehutgroup.com>
2021-07-22 16:35:16 +0100

    check BST.  replace boundary method

--- top-interview-questions/Easy/4-Trees/2-Validate Binary Search Tree.py
+++ top-interview-questions/Easy/4-Trees/2-Validate Binary Search Tree.py
@@ -5,18 +5,12 @@
 #         self.left = left
 #         self.right = right
 
-def in_order(root,visit):
-    if root:
-        in_order(root.left,visit)
-        visit.append(root.val)
-        # print("visited -> ", root.val)
-        in_order(root.right,visit)
-    
-class Solution:
-    def isValidBST(self, root: TreeNode) -> bool:
-        visit = [] 
-        in_order(root,visit)
-        for i in range(len(visit)-1):
-            if visit[i] >= visit[i+1]:
-                return False
-        return True
+def is_bi_search_tree(root):
+    def help(root, lower=float('-inf'), upper=float('inf')):
+        if not root:
+            return True
+        if root.val < lower or root.val > upper:
+            return False
+        return help(root.left, lower, root.val) and help(root.right, root.val, upper)
+
+    return help(root)
